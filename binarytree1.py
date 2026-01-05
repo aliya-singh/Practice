9f52c805e2b7bdf26f04ec3acb725f80b610dd10
aliya-singh <aliyasingh51564@gmail.com>
2026-01-05 20:57:55 +0530

    Adding Company Hirarchy

--- binarytree1.py
+++ binarytree1.py
@@ -3,50 +3,77 @@ class TreeNode:
         self.data = data
         self.children = []
         self.parent = None
-
+    
     def add_child(self, child):
         child.parent = self
         self.children.append(child)
     
+    def print_tree_name(self):
+        spaces = "  " * self.get_level()
+        prefix = spaces + "|__" if self.parent else ""
+        for key in self.data.keys():
+            print(prefix, key)
+        if self.children:
+            for child in self.children:
+                child.print_tree_name()
+    
+    def print_tree_desg(self):
+        space = "  " * self.get_level()
+        prefix = space + "|__" if self.parent else ""
+        for value in self.data.values():
+            print(prefix, value)
+        
+        if self.children:
+            for child in self.children:
+                child.print_tree_desg()
+
     def print_tree(self):
-        space = " " * self.get_level()
-        prifix = space + "|__" if self.parent else :
-        print(self.data)
+        space = "  " * self.get_level()
+        prefix = space + "|__" if self.parent else ""
+        for key, value in self.data.items():
+            print(prefix, key, "(", value, ")")
+        
         if self.children:
             for child in self.children:
                 child.print_tree()
-    
+
     def get_level(self):
         level = 1
         p = self.parent
         while p:
-            level += 1
             p = p.parent
-        return level
+            level += 1
         
-def buit_tree():
-    root = TreeNode("Electronics")
+        return level
 
-    laptop = TreeNode("Laptop")
-    
-    laptop.add_child(TreeNode("Mac"))
-    laptop.add_child(TreeNode("HP"))
+def create_tree():
+    ceo = TreeNode({"Nilupul":"CEO"})
+    cto = TreeNode({"Chinmay":"CTO"})
+    ih = TreeNode({"Vishwa":"Infrastruture Head"})
+    cm = TreeNode({"Dhaval":"Cloud Maneger"})
+    am = TreeNode({"Abhijeet":"App Maneger"})
+    ah = TreeNode({"Aamir":"Application Head"})
+    hr = TreeNode({"Gels":"HR Head"})
+    rm = TreeNode({"Peter":"Recrutment Maneger"})
+    pm = TreeNode({"Waqas":"Policy Maneger"})
 
-    phone = TreeNode("Phone")
-    phone.add_child(TreeNode("Nokia"))
-    phone.add_child(TreeNode("Samsung"))
-    phone.add_child(TreeNode("iPhone"))
+    ceo.add_child(cto)
+    ceo.add_child(hr)
 
-    root.add_child(laptop)
-    root.add_child(phone)
+    cto.add_child(ih)
+    cto.add_child(ah)
 
-    return root
+    ih.add_child(cm)
+    ih.add_child(am)
 
+    hr.add_child(rm)
+    hr.add_child(pm)
 
-
+    return ceo
 
 
 if __name__ == "__main__":
-    root = buit_tree()
-    root.print_tree()
-    
+    ceo = create_tree()
+    ceo.print_tree_name()
+    ceo.print_tree_desg()
+    ceo.print_tree()
