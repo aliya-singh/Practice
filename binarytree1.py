35d7e7980c973da82c0cbfef0d4cbf77c2e38790
aliya-singh <aliyasingh51564@gmail.com>
2026-01-05 22:44:58 +0530

    YouTube

--- binarytree1.py
+++ binarytree1.py
@@ -1,6 +1,7 @@
 class TreeNode:
-    def __init__(self, data):
+    def __init__(self, data, desg):
         self.data = data
+        self.desg = desg
         self.children = []
         self.parent = None
     
@@ -8,34 +9,20 @@ class TreeNode:
         child.parent = self
         self.children.append(child)
     
-    def print_tree_name(self):
+    def print_tree(self, choice):
         spaces = "  " * self.get_level()
         prefix = spaces + "|__" if self.parent else ""
-        for key in self.data.keys():
-            print(prefix, key)
-        if self.children:
-            for child in self.children:
-                child.print_tree_name()
-    
-    def print_tree_desg(self):
-        space = "  " * self.get_level()
-        prefix = space + "|__" if self.parent else ""
-        for value in self.data.values():
-            print(prefix, value)
-        
-        if self.children:
-            for child in self.children:
-                child.print_tree_desg()
+        if choice == "names":
+            print(prefix, self.data)
+        elif choice == "both":
+            print(prefix, self.data, "(", self.desg, ")")
+        else:
+            print(prefix, self.desg)
 
-    def print_tree(self):
-        space = "  " * self.get_level()
-        prefix = space + "|__" if self.parent else ""
-        for key, value in self.data.items():
-            print(prefix, key, "(", value, ")")
-        
         if self.children:
             for child in self.children:
-                child.print_tree()
+                child.print_tree(choice)
+    
 
     def get_level(self):
         level = 1
@@ -47,15 +34,15 @@ class TreeNode:
         return level
 
 def create_tree():
-    ceo = TreeNode({"Nilupul":"CEO"})
-    cto = TreeNode({"Chinmay":"CTO"})
-    ih = TreeNode({"Vishwa":"Infrastruture Head"})
-    cm = TreeNode({"Dhaval":"Cloud Maneger"})
-    am = TreeNode({"Abhijeet":"App Maneger"})
-    ah = TreeNode({"Aamir":"Application Head"})
-    hr = TreeNode({"Gels":"HR Head"})
-    rm = TreeNode({"Peter":"Recrutment Maneger"})
-    pm = TreeNode({"Waqas":"Policy Maneger"})
+    ceo = TreeNode("Nilupul", "CEO")
+    cto = TreeNode("Chinmay", "CTO")
+    ih = TreeNode("Vishwa", "Infrastruture Head")
+    cm = TreeNode("Dhaval", "Cloud Maneger")
+    am = TreeNode("Abhijeet", "App Maneger")
+    ah = TreeNode("Aamir", "Application Head")
+    hr = TreeNode("Gels", "HR Head")
+    rm = TreeNode("Peter", "Recrutment Maneger")
+    pm = TreeNode("Waqas", "Policy Maneger")
 
     ceo.add_child(cto)
     ceo.add_child(hr)
@@ -74,6 +61,6 @@ def create_tree():
 
 if __name__ == "__main__":
     ceo = create_tree()
-    ceo.print_tree_name()
-    ceo.print_tree_desg()
-    ceo.print_tree()
+    ceo.print_tree("desg")
+    ceo.print_tree("names")
+    ceo.print_tree("both")
